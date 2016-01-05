6a67dd93d69950e616c59a48c8332daa7443557b
Tongxi Lou <tongxi.lou@tum.de>
2016-01-05 23:35:11 +0100

    tried to support hsv space, but the functions should be correct, a way to call function in RenderScript is needed. TODO

--- app/src/main/rs/imgproc.rs
+++ app/src/main/rs/imgproc.rs
@@ -29,6 +29,77 @@ int radius;
 float scale;
 float threshold_value;
 
+/*float3 __attribute__((kernel)) rgb_to_hsv(float3 input) {
+    float3 hsv;
+    float c_min = 2.0f, c_max = -1.0f, delta;
+    int id_max;
+    for(int i = 0; i < 3; i++) {
+        if(c_max < input[i]) {
+            id_max = i;
+            c_max = input[i];
+        }
+        if(c_min > input[i]) {
+            c_max = input[i];
+        }
+    }
+    c_max = max(input[0], max(input[1], input[2]));
+    c_min = min(input[0], min(input[1], input[2]));
+    delta = c_max - c_min;
+
+    if(delta < 0.000000001f || c_max < 0.000000001f) {
+        hsv[0] = 0.0f;
+        hsv[1] = 0.0f;
+        hsv[2] = c_max;
+    }
+    else {
+        hsv[0] = 60.0f * (((float)id_max) * 2.0f
+            * (float)(input[(id_max + 1) % 3] - input[(id_max + 2) % 3]) / delta);
+        if(hsv[0] < 0.0f) {
+            hsv[0] += 360.0f;
+        }
+        hsv[1] = delta / c_max;
+        hsv[2] = c_max;
+    }
+
+    return hsv;
+}
+
+float3 __attribute__((kernel)) hsv_to_rgb(float3 input) {
+    float3 rgb;
+    int i;
+    float f, p, q, t;
+    float3 tmp;
+
+    if( input[1] < 0.000000001f ) {
+    	rgb = input[1];
+    }
+    else {
+    	input[0] /= 60.0f;
+    	i = (int)floor(input[0]);
+    	f = input[0] - floor(input[0]);
+
+    	p = input[2] * (1.0f - input[1]);
+    	q = input[2] * (1.0f - input[1] * f);
+    	t = input[2] * (1.0f - input[1] * (1.0f - f));
+
+        if(i % 2 == 0) {
+            tmp = (float3)(input[2], t, p);
+            i /= 2;
+            rgb[0] = tmp[i];
+            rgb[1] = tmp[(i + 1) % 3];
+            rgb[2] = tmp[(i + 2) % 3];
+        }
+        else {
+            tmp = (float3)(q, input[2], p);
+            i /= 2;
+            rgb[0] = tmp[i];
+            rgb[1] = tmp[(i + 1) % 3];
+            rgb[2] = tmp[(i + 2) % 3];
+        }
+    }
+    return rgb;
+}*/
+
 uchar4 __attribute__((kernel)) out_to_in(uchar4 in) {
     return in;
 }
@@ -156,12 +227,18 @@ uchar4 __attribute__((kernel)) threshold(uchar4 in) {
     float4 result;
     float4 f4 = rsUnpackColor8888(in);
     if(if_bw || index_channel < 0 || index_channel >= 4) {
+    // if(if_bw || index_channel < 0 || index_channel >= 7) {
         float value = 0.2126f * f4.x + 0.7152f * f4.y + 0.0722f * f4.z;
         if_over = value > threshold_value;
     }
-    else {
+    else if(index_channel < 4) {
+        // r, g, b or alpha
         if_over = f4[index_channel] > threshold_value;
     }
+    // else {
+        // h, s or v
+        // if_over = rgb_to_hsv(f4.xyz)[index_channel - 4] > threshold_value;
+    // }
 
     if(if_over) {
         result = (float4)(1.0f, 1.0f, 1.0f, 1.0f);
